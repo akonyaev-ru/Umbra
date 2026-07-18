2d1ec116013732d089e07bced9183218f3ac563e
akonyaev-ru <akonyaev-ru@users.noreply.github.com>
2026-07-18 18:20:01 +0300

    Fix CWD path resolution for models

--- src/nlp_engine.py
+++ src/nlp_engine.py
@@ -10,7 +10,7 @@ def get_resource_path(relative_path):
         # PyInstaller creates a temp folder and stores path in _MEIPASS
         base_path = sys._MEIPASS
     except AttributeError:
-        base_path = os.path.abspath(".")
+        base_path = os.path.dirname(os.path.abspath(__file__))
     
     return os.path.join(base_path, relative_path)
 
